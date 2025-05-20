d65517926fbe35b5633f18765e9bc25e55f3ef58
Neal Richardson <neal.p.richardson@gmail.com>
2025-05-20 08:09:52 -0400

    Move/rename bundle_dir input validation examples

--- tests/testthat/test-deploy.R
+++ tests/testthat/test-deploy.R
@@ -1,7 +1,8 @@
 test_that("bundle_dir errors if no manifest.json", {
   expect_error(
     suppressMessages(bundle_dir(rprojroot::find_testthat_root_file(
-      "examples/no_manifest"
+      "bundle_dir_tests",
+      "no_manifest"
     ))),
     "no `manifest.json`"
   )
@@ -10,7 +11,7 @@ test_that("bundle_dir errors if no manifest.json", {
 test_that("bundle_dir warns if packrat.lock", {
   expect_warning(
     suppressMessages(bundle_dir(rprojroot::find_testthat_root_file(
-      "examples",
+      "bundle_dir_tests",
       "include_packrat_lock"
     ))),
     "`packrat.lock` file found"
@@ -20,7 +21,7 @@ test_that("bundle_dir warns if packrat.lock", {
 test_that("bundle_dir warns if packrat folder", {
   expect_warning(
     suppressMessages(bundle_dir(rprojroot::find_testthat_root_file(
-      "examples",
+      "bundle_dir_tests",
       "include_packrat"
     ))),
     "`packrat` directory found"
@@ -30,8 +31,8 @@ test_that("bundle_dir warns if packrat folder", {
 test_that("bundle_dir not fooled by subfolders", {
   expect_error(
     suppressMessages(bundle_dir(rprojroot::find_testthat_root_file(
-      "examples",
-      "include_multiple_folders"
+      "bundle_dir_tests",
+      "subfolders"
     ))),
     "no `manifest.json`"
   )
@@ -39,7 +40,7 @@ test_that("bundle_dir not fooled by subfolders", {
 
 test_that("bundle_dir works with relative paths", {
   bnd <- suppressMessages(bundle_dir(fs::path_rel(rprojroot::find_testthat_root_file(
-    "examples",
+    "bundle_dir_tests",
     "doc"
   ))))
   expect_true(fs::file_exists(bnd$path))
@@ -49,7 +50,7 @@ test_that("bundle_dir errors for nonexistent paths", {
   expect_error(
     suppressMessages(
       bundle_dir(rprojroot::find_testthat_root_file(
-        "examples",
+        "bundle_dir_tests",
         "does_not_exist_not_real"
       ))
     ),
